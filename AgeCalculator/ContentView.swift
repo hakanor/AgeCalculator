1301d5b6ebad7ad0e2bb31d0a134f30b9bfa5f47
Hakan Or <hakanor@Hakans-MacBook-Pro.local>
2024-01-21 23:28:28 +0300

    feature: show age with transistion

--- AgeCalculator/ContentView.swift
+++ AgeCalculator/ContentView.swift
@@ -10,13 +10,12 @@ import SwiftUI
 struct ContentView: View {
     @State var birthDate = ISO8601DateFormatter().date(from: "1999-01-20T19:20:46+0000")!
     @State var age: Double = 0
-    @State var ageToShow: Double = 0
     let timerInterval = 1.0
     
     var ageFormatter: NumberFormatter = {
         var formatter = NumberFormatter()
         formatter.numberStyle = .decimal
-        formatter.minimumFractionDigits = 4 // Küsüratlı kısmın minimum 4 hane olması için
+        formatter.minimumFractionDigits = 7
         return formatter
     }()
     
@@ -28,32 +27,20 @@ struct ContentView: View {
                 .bold()
                 .onAppear {
                     Timer.scheduledTimer(withTimeInterval: timerInterval, repeats: true) { timer in
-                        age = calculateAgeInSeconds(from: birthDate)
-                        incrementAge(with: timerInterval)
-                        
+                        withAnimation {
+                            self.age = calculateAge(from: birthDate) / 31556926
+                        }
                     }
                 }
                 .lineLimit(1)
-            
-            Button {
-                self.incrementAge(with: 1)
-            } label: {
-                Text("Get older")
-            }
         }
         .padding()
     }
     
-    func calculateAgeInSeconds(from birthDate: Date) -> Double {
+    func calculateAge(from birthDate: Date) -> Double {
         let currentTime = Date()
         let timeDifference = currentTime.timeIntervalSince(birthDate)
         return timeDifference
-    }
-    
-    func incrementAge(with amount: Double) {
-        withAnimation {
-            self.age += amount
-        }
     }
 }
 
