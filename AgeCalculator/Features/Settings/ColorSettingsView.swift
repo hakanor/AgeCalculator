31397ba7cb48bcf60196224f804e1d2e04cf8b86
hakanor <hakanor99@gmail.com>
2024-03-06 21:15:50 +0300

    feature: change button color

--- AgeCalculator/Features/Settings/ColorSettingsView.swift
+++ AgeCalculator/Features/Settings/ColorSettingsView.swift
@@ -8,7 +8,57 @@
 import SwiftUI
 
 struct ColorSettingsView: View {
+    
+    @State private var redValue: Double = 0.5
+    @State private var greenValue: Double = 0.5
+    @State private var blueValue: Double = 0.5
+    
+    let basicColors: [Color] = [.red, .green, .blue, .yellow, .orange, .purple]
+ 
     var body: some View {
-        Text("color change screen")
+        VStack {
+            HStack {
+                Text("Red")
+                SliderView(value: $redValue, sliderRange: 0...1, thumbColor: Color(red: redValue, green: greenValue, blue: blueValue))
+                    .frame(height: 30)
+            }
+            
+            HStack {
+                Text("Blue")
+                SliderView(value: $blueValue, sliderRange: 0...1, thumbColor: Color(red: redValue, green: greenValue, blue: blueValue))
+                    .frame(height: 30)
+            }
+            
+            HStack {
+                Text("Green")
+                SliderView(value: $greenValue, sliderRange: 0...1, thumbColor: Color(red: redValue, green: greenValue, blue: blueValue))
+                    .frame(height: 30)
+            }
+            
+            HStack {
+                ForEach(basicColors, id: \.self) { color in
+                    Circle()
+                        .fill(color)
+                        .frame(width: 30, height: 30)
+                        .onTapGesture {
+                            let uiColor = UIColor(color)
+                            redValue = Double(uiColor.cgColor.components?[0] ?? 0)
+                            greenValue = Double(uiColor.cgColor.components?[1] ?? 0)
+                            blueValue = Double(uiColor.cgColor.components?[2] ?? 0)
+                        }
+                }
+            }
+            .padding()
+            
+            Button(action: {
+                // burada renk kayıt etme ve kapatma işlemleri olacak
+            }, label: {
+                RetroView(type: .text("apply_string"),size: 16, backgroundColor: Color(red: redValue, green: greenValue, blue: blueValue))
+                    .fixedSize()
+            })
+            .buttonStyle(RetroButtonStyle())
+            
+        }
+        .padding()
     }
 }
---
+++ AgeCalculator/Views/SliderView.swift
@@ -0,0 +1,80 @@
+//
+//  SliderView.swift
+//  AgeCalculator
+//
+//  Created by Hakan Or on 6.03.2024.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    @Binding var value: Double
+    
+    @State var lastCoordinateValue: CGFloat = 0.0
+    var sliderRange: ClosedRange<Double> = 1...100
+    var thumbColor: Color = Color(.systemGray)
+    var minTrackColor: Color = Color(.systemGray3)
+    var maxTrackColor: Color = Color(.systemGray4)
+    
+    var body: some View {
+        GeometryReader { gr in
+            let thumbHeight = gr.size.height * 1.1
+            let thumbWidth = gr.size.width * 0.03
+            let radius = gr.size.height * 0.5
+            let minValue = gr.size.width * 0.015
+            let maxValue = (gr.size.width * 0.98) - thumbWidth
+            
+            let scaleFactor = (maxValue - minValue) / (sliderRange.upperBound - sliderRange.lowerBound)
+            let lower = sliderRange.lowerBound
+            let sliderVal = (self.value - lower) * scaleFactor + minValue
+            
+            ZStack {
+                
+                Rectangle()
+                    .foregroundColor(maxTrackColor)
+                    .frame(width: gr.size.width, height: gr.size.height * 0.95)
+                    .clipShape(RoundedRectangle(cornerRadius: radius))
+                HStack {
+                    Rectangle()
+                        .foregroundColor(minTrackColor)
+                    .frame(width: sliderVal, height: gr.size.height * 0.95)
+                    Spacer()
+                }
+                .clipShape(RoundedRectangle(cornerRadius: radius))
+                HStack {
+                    ZStack {
+                        
+                        // Top Label
+                        RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(thumbColor)
+                        
+                        // Stroke
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(style: StrokeStyle(lineWidth: 2.5, lineCap: .round, lineJoin: .round))
+                            .colorMultiply(ThemeColors.strokeColor)
+                    }
+
+                        .foregroundColor(thumbColor)
+                        .frame(width: thumbWidth, height: thumbHeight)
+                        .offset(x: sliderVal)
+                        .gesture(
+                            DragGesture(minimumDistance: 0)
+                                .onChanged { v in
+                                    if (abs(v.translation.width) < 0.1) {
+                                        self.lastCoordinateValue = sliderVal
+                                    }
+                                    if v.translation.width > 0 {
+                                        let nextCoordinateValue = min(maxValue, self.lastCoordinateValue + v.translation.width)
+                                        self.value = ((nextCoordinateValue - minValue) / scaleFactor)  + lower
+                                    } else {
+                                        let nextCoordinateValue = max(minValue, self.lastCoordinateValue + v.translation.width)
+                                        self.value = ((nextCoordinateValue - minValue) / scaleFactor) + lower
+                                    }
+                               }
+                        )
+                    Spacer()
+                }
+            }
+        }
+    }
+}
